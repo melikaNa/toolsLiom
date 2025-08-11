2f7391759329e4ecf7cac22c9d33d43be079613d
Melika <52023243+melikaNa@users.noreply.github.com>
2025-08-11 20:52:45 +0330

    Update plasmic-host.tsx

--- src/pages/plasmic-host.tsx
+++ src/pages/plasmic-host.tsx
@@ -18,6 +18,8 @@ import { ApiRequest, apiRequestMeta } from "@/fragment/components/api-request";
 import { Slider, sliderMeta } from "@/fragment/components/slider";
 import { Chart, chartMeta } from "@/fragment/components/chart";
 import { Textarea, textareaMeta } from "@/fragment/components/textarea";
+import {BackHandler,BackHandlerMeta} from "@/components/BackHandler";
+
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
@@ -36,3 +38,4 @@ registerComponent(Slider, sliderMeta);
 registerComponent(ApiRequest, apiRequestMeta);
 registerComponent(Chart, chartMeta);
 registerComponent(Textarea, textareaMeta);
+registerComponent(BackHandler,BackHandlerMeta );
